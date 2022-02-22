e0769e5b5b8156649933933681fcbd909f95551c
wmaf <wmaf@outlook.com>
2022-02-22 16:37:18 -0500

    Code update, pratice Exception handler

--- src/com/careerdevs/ui/UserInterface.java
+++ src/com/careerdevs/ui/UserInterface.java
@@ -6,21 +6,22 @@ import java.util.Scanner;
 public class UserInterface {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String readStrung (String question){  //the call is  userInput.readString
-        System.out.println(question+"\nInput: ");
+    public static String readStrung(String question) {  //the call is  userInput.readString
+        System.out.println(question + "\nInput: ");
         return scanner.nextLine();
 
     }
-    public static int readInt (String question) {
-        while (true) {
-            System.out.println(question+"\nNumber:");
-        try {
-            int temp = scanner.nextInt();
-            return temp;
 
-        } catch (InputMismatchException e) {
-            scanner.nextLine();
-            System.out.println("You must enter");
-            return scanner.nextInt();
+    public static int readInt(String question) {
+        while (true) {
+            try {
+                System.out.println(question + "\nNumber:");
+                return scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter a number, please try again.");
+                scanner.nextLine();
+            }
+        }
     }
 }
